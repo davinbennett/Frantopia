446b5a12f68c98f6722a716ec416e8c940a7ee18
davin bennett <davinbennet99@gmail.com>
2024-11-09 03:48:43 +0700

    Co-authored-by: ALEXIS HIDAYAT <alexis.hidayat@binus.ac.id>

---
+++ frantopia-frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./App.jsx",
+    "./src/presentation/screens/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [ require( "nativewind/preset" ) ],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#2D70F3',
+        blueDark: '#062DF6',
+        grayLight: '#D9D9D9',
+        gray: '#D0D0D0',
+        grayDark: 'BFBFBF',
+        background: '#F3F4FE',
+      },
+    },
+  },
+  plugins: [],
+};
